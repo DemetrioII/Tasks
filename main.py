6851b02912dde6689cc8c2aeaad6d10954fa45fe
demetrio <lisovd2006@yandex.ru>
2022-02-18 07:44:10 +0300

    Task7

--- main.py
+++ main.py
@@ -14,6 +14,24 @@ def index():
     return f"<h3>И на Марсе будут яблони цвести!</h3>"
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                    </head>
+                    <body>
+                        <h2>Результаты отбора</h2>
+                        <h3>Претендента на участие в мисси {nickname}:</h3>
+                        <div class="alert alert-primary">Поздравляем! Ваш рейтинг полсле {level} этапа отбора</div>
+                        <div>составляет {rating}!</div>
+                        <div class="alert alert-warning">Желаем удачи!</div>
+                    </body>
+                </html>'''
+
+
 @app.route('/promotion')
 def promotion():
     return f"Человечество вырастает из детства. <br> Человечеству мала одна планета. <br> " \
